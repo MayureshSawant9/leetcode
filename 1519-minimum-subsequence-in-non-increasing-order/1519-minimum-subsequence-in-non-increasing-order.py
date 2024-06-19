093a932255f3bdeeb4a2a590e71c7424a26da00a
MayureshSawant9 <122062201+MayureshSawant9@users.noreply.github.com>
2024-06-19 12:09:51 +0530

    Time: 47 ms (98.07%), Space: 16.6 MB (17.37%) - LeetHub

---
+++ 1519-minimum-subsequence-in-non-increasing-order/1519-minimum-subsequence-in-non-increasing-order.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSubsequence(self, nums: List[int]) -> List[int]:
+        nums.sort(reverse=True)
+        sum_half = sum(nums)/2
+        sub_sum = 0
+        for index, element in enumerate(nums):
+            sub_sum += element
+            if sub_sum > sum_half:
+                return nums[:index + 1]
+
+
+
+        
