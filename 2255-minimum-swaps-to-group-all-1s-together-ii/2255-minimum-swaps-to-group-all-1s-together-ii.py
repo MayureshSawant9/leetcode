dc83185eb863365b965753814c70f1295b215316
MayureshSawant9 <122062201+MayureshSawant9@users.noreply.github.com>
2024-08-02 17:44:50 +0530

    Time: 679 ms (76.35%), Space: 20.6 MB (37.84%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.py
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.py
@@ -2,25 +2,14 @@ class Solution:
     def minSwaps(self, nums: List[int]) -> int:
 
         win_size = nums.count(1)
-        nums.extend(nums[:win_size])
-        max_ones = float('-inf')
-        one_count = 0
-        for i in range(win_size):
-            if nums[i]:
-                one_count += 1
+        # nums.extend(nums[:win_size])
+        one_count = sum(nums[:win_size])
+        max_ones = one_count
+        n = len(nums)
 
-        print()
-
-        i, j = 0, win_size
-
-        while j < len(nums):
-                
-            if not nums[i] and nums[j]:
-                one_count += 1
-            elif nums[i] and not nums[j]:
-                one_count -= 1
+        for i in range(1, n):
+            
+            one_count += nums[(i + win_size) % n - 1] - nums[i-1]
             max_ones = max(max_ones, one_count)
-            i += 1
-            j += 1
-
+                    
         return win_size - max_ones
